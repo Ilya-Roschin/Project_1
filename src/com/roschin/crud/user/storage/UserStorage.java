07dd4294c89acb0d3562174e03a3d7c1e7d8573e
Ilya-Roschin <roshhin-78@mail.ru>
2021-09-03 00:25:03 +0300

    refactor user storage

--- src/com/roschin/crud/user/storage/UserStorage.java
+++ src/com/roschin/crud/user/storage/UserStorage.java
@@ -2,6 +2,8 @@ package com.roschin.crud.user.storage;
 
 import com.roschin.crud.user.model.User;
 
+import java.util.Random;
+
 public class UserStorage {
 
     private static final int ARRAY_SIZE = 10;
@@ -29,16 +31,23 @@ public class UserStorage {
 
     public User findByFirstName(String firstName) {
         if (firstName != null) {
-            for (User user : storage) {
-                if (firstName.equals(user.getFirstName())) {
-                    return user;
+            int i = 0;
+            while (i < ARRAY_SIZE) {
+                if (storage[i] == null) {
+                    i++;
+                    continue;
                 }
+                if (firstName.equals(storage[i].getFirstName())) {
+                   return storage[i];
+                }
+                i++;
             }
         }
         throw new RuntimeException("There is no user with given username: " + firstName);
     }
 
     public void deleteByUserName(String firstName) {
+        boolean check = true;
         if (firstName != null) {
             int i = 0;
             while (i < ARRAY_SIZE) {
@@ -48,11 +57,36 @@ public class UserStorage {
                 }
                 if (storage[i].getFirstName().equals(firstName)) {
                     storage[i] = null;
+                    System.out.println("User deleted! ");
+                    check = false;
                     break;
                 }
                 i++;
             }
         }
+        if (check) {
+            System.out.println("User not found! ");
+        }
+    }
+
+    public int createNewUserId() {
+        Random random = new Random();
+        boolean check;
+        int newUserIndex;
+        do {
+            check = false;
+            newUserIndex = 100000 + random.nextInt(899999);
+            for (User element : storage) {
+                if (element == null) {
+                    continue;
+                }
+                if (element.getId() == newUserIndex) {
+                    check = true;
+                    break;
+                }
+            }
+        } while (check);
+        return newUserIndex;
     }
 
     public User[] findAll() {
