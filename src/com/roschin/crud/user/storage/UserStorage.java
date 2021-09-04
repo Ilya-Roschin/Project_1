a31c33ea36e9d13a19fec6668efa4b4584651ff1
Ilya-Roschin <roshhin-78@mail.ru>
2021-09-04 14:08:30 +0300

    refactor project

--- src/com/roschin/crud/user/storage/UserStorage.java
+++ src/com/roschin/crud/user/storage/UserStorage.java
@@ -2,75 +2,71 @@ package com.roschin.crud.user.storage;
 
 import com.roschin.crud.user.model.User;
 
-import java.util.Random;
+public class UserStorage {
 
-    public class UserStorage {
+    private static final int ARRAY_SIZE = 10;
+    private final User[] storage = new User[ARRAY_SIZE];
 
-        private static final int ARRAY_SIZE = 10;
-        private final User[] storage = new User[ARRAY_SIZE];
-
-        public void add(User user) {
-            int indexOfEmptyElement = findIndexOfEmptyElement();
-            if (indexOfEmptyElement != -1) {
-                storage[indexOfEmptyElement] = user;
-            } else {
-                System.out.println("no place for users");
-            }
-        }
-
-        private int findIndexOfEmptyElement() {
-            int index = 0;
-            while (index < ARRAY_SIZE) {
-                if (storage[index] == null) {
-                    return index;
-                }
-                index++;
-            }
-            return -1;
-        }
-
-        public User findByFirstName(String firstName) {
-            if (firstName != null) {
-                int i = 0;
-                while (i < ARRAY_SIZE) {
-                    if (storage[i] == null) {
-                        i++;
-                        continue;
-                    }
-                    if (firstName.equals(storage[i].getFirstName())) {
-                        return storage[i];
-                    }
-                    i++;
-                }
-            }
-            throw new RuntimeException("There is no user with given username: " + firstName);
-        }
-
-        public void deleteByUserName(String firstName) {
-            boolean check = true;
-            if (firstName != null) {
-                int i = 0;
-                while (i < ARRAY_SIZE) {
-                    if (storage[i] == null) {
-                        i++;
-                        continue;
-                    }
-                    if (storage[i].getFirstName().equals(firstName)) {
-                        storage[i] = null;
-                        System.out.println("User deleted! ");
-                        check = false;
-                        break;
-                    }
-                    i++;
-                }
-            }
-            if (check) {
-                System.out.println("User not found! ");
-            }
-        }
-
-        public User[] findAll() {
-            return storage;
+    public void add(User user) {
+        int indexOfEmptyElement = findIndexOfEmptyElement();
+        if (indexOfEmptyElement != -1) {
+            storage[indexOfEmptyElement] = user;
+        } else {
+            System.out.println("no place for users");
         }
     }
+
+    public User findByFirstName(String firstName) {
+        int i = 0;
+        while (i < ARRAY_SIZE) {
+            if (storage[i] == null) {
+                i++;
+                continue;
+            }
+            if (storage[i].getFirstName().equals(firstName)) {
+                return storage[i];
+            }
+            i++;
+        }
+        throw new RuntimeException("There is no user with given username: " + firstName);
+    }
+
+    public void deleteByUserName(String firstName) {
+        boolean check = true;
+        if (firstName != null) {
+            int i = 0;
+            while (i < ARRAY_SIZE) {
+                if (storage[i] == null) {
+                    i++;
+                    continue;
+                }
+                if (storage[i].getFirstName().equals(firstName)) {
+                    storage[i] = null;
+                    System.out.println("User deleted! ");
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+        }
+        if (check) {
+            System.out.println("User not found! ");
+        }
+    }
+
+    public User[] findAll() {
+        return storage;
+    }
+
+    private int findIndexOfEmptyElement() {
+        int index = 0;
+        while (index < ARRAY_SIZE) {
+            if (storage[index] == null) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+}
 
